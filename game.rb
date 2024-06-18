948d7a42fdd6f7d7494e2af246f7abba4f48aa07
boulhouech <slim.boulahouech@gmail.com>
2024-06-18 04:36:37 +0100

    create the Game class

--- game.rb
+++ game.rb
@@ -1,13 +1,25 @@
-def user_option(option)
-  @option = option
-  until ['1', '2'].include?(@option)
-    puts 'Invalid choice. Please choose 1 or 2.'
+
+
+class Game
+  attr_accessor :correct_guesses, :remaining_attempts
+  attr_reader :option
+
+  def initialize
+    @correct_guesses = []
+    @remaining_attempts = 10
   end
 
-  case @option
-  when '1'
-    start_new_game
-  when '2'
-    load_saved_game
+  def user_option(option)
+    @option = option
+    until ['1', '2'].include?(@option)
+      puts 'Invalid choice. Please choose 1 or 2.'
+    end
+
+    case @option
+    when '1'
+      start_new_game
+    when '2'
+      load_saved_game
+    end
   end
 end
